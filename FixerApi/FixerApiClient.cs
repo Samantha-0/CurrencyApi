22f6b09d33c8a583f204cae65739eaea0d413ae4
Samantha <samantha.speksnijder@gmail.com>
2022-06-12 16:46:12 +0200

    Add Fixer API project

---
+++ FixerApi/FixerApiClient.cs
@@ -0,0 +1,59 @@
+﻿using Common.Input;
+using Common.Models;
+using Common.ResponseModel;
+using Newtonsoft.Json;
+
+namespace FixerApi;
+
+public class FixerApiClient : IFixerApiClient
+{
+    readonly IFixerRestClient _fixerRestClient;
+
+    public FixerApiClient(IFixerRestClient fixerRestClient)
+    {
+        _fixerRestClient = fixerRestClient;
+    }
+
+    public List<CurrencySymbol> GetSymbols()
+    {
+        var response = _fixerRestClient.ExecuteRequest("symbols");
+        var symbolResponse = JsonConvert.DeserializeObject<SymbolResponse>(response);
+
+        var symbolList = new List<CurrencySymbol>();
+        foreach (var item in symbolResponse.Symbols)
+            symbolList.Add(new CurrencySymbol
+            {
+                Code = item.Name,
+                Name = (string)item.First
+            });
+
+        return symbolList;
+    }
+
+    public List<CurrencyOnDate> GetLatest(ExchangeRateInput input)
+    {
+        var response = _fixerRestClient.ExecuteRequest($"latest?symbols={input.ToCurrency}&base={input.FromCurrency}");
+        var latestResponse = JsonConvert.DeserializeObject<LatestResponse>(response);
+
+        var symbolList = new List<CurrencyOnDate>();
+        foreach (var item in latestResponse.Rates)
+            symbolList.Add(new CurrencyOnDate
+                {
+                    FromCurrency = item.Name,
+                    ToCurrency = input.ToCurrency,
+                    Date = input.Date,
+                    Value = (decimal)item.First
+                }
+            );
+
+        return symbolList;
+    }
+    
+    // TODO - Part 5 - would be a new function here
+}
+
+public interface IFixerApiClient
+{
+    List<CurrencySymbol> GetSymbols();
+    List<CurrencyOnDate> GetLatest(ExchangeRateInput input);
+}
---
+++ FixerApi/FixerRestClient.cs
@@ -0,0 +1,39 @@
+﻿using Common.Config;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RestSharp;
+
+namespace FixerApi;
+
+public class FixerRestClient : IFixerRestClient
+{
+    readonly FixerApiOptions _fixerApiOptions;
+    readonly ILogger<FixerRestClient> _logger;
+
+    public FixerRestClient(ILogger<FixerRestClient> logger, IOptions<FixerApiOptions> fixerApiOptions)
+    {
+        _fixerApiOptions = fixerApiOptions.Value;
+        _logger = logger;
+    }
+
+    public string? ExecuteRequest(string resource)
+    {
+        var client = new RestClient(_fixerApiOptions.BaseUrl);
+
+        var request = new RestRequest(resource);
+        request.AddHeader("apikey", _fixerApiOptions.ApiKey);
+
+        var response = client.Execute(request);
+
+        if (response.IsSuccessful)
+            return response.Content;
+
+        _logger.LogError(response.ErrorException, "Unsuccessful request to {Resource}", resource);
+        throw new Exception($"Unsuccessful request to {resource}");
+    }
+}
+
+public interface IFixerRestClient
+{
+    public string? ExecuteRequest(string resource);
+}
