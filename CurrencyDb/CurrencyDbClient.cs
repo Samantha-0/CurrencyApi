f09718e12014325fcc2d8bf10fb0767569a4a8a2
Samantha <samantha.speksnijder@gmail.com>
2022-06-12 16:46:31 +0200

    Add Currency DB project

---
+++ CurrencyDb/CurrencyDbClient.cs
@@ -0,0 +1,36 @@
+﻿using Common.Input;
+using Common.Models;
+
+namespace CurrencyDb;
+
+/// <summary>
+/// TODO: Class/project intended to access the database.
+/// </summary>
+public class CurrencyDbClient : ICurrencyDbClient
+{
+    public List<CurrencySymbol>? GetSymbols()
+    {
+        return null;
+    }
+
+    public void SaveSymbols(List<CurrencySymbol> symbolList)
+    {
+    }
+
+    public CurrencyOnDate? GetLatest(ExchangeRateInput exchangeRateInput)
+    {
+        return null;
+    }
+
+    public void SaveLatest(List<CurrencyOnDate> latestCurrencies)
+    {
+    }
+}
+
+public interface ICurrencyDbClient
+{
+    List<CurrencySymbol>? GetSymbols();
+    void SaveSymbols(List<CurrencySymbol> symbolList);
+    CurrencyOnDate? GetLatest(ExchangeRateInput exchangeRateInput);
+    void SaveLatest(List<CurrencyOnDate> latestCurrencies);
+}
